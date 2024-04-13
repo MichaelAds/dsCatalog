30fea4577ec81f651fcda9a62ba74e937dea53b4
Michael Souza <hyroiu@Laptop-de-Michael.local>
2024-04-12 23:36:21 -0300

    refactor pageable

--- backend/src/main/java/com/javaexpert/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/javaexpert/dscatalog/services/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -77,8 +78,8 @@ public class ProductService {
         }
     }
 
-    public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-        Page<Product> list = productRepository.findAll(pageRequest);
+    public Page<ProductDTO> findAllPaged(Pageable pageable) {
+        Page<Product> list = productRepository.findAll(pageable);
         return list.map(ProductDTO::new);
     }
 
